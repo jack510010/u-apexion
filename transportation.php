c6176bacc4bf51520aa159614b0a88574c704310
Michael Chen <michaelexgt@gmail.com>
2022-01-03 14:40:10 +0800

    new proj

---
+++ transportation.php
@@ -0,0 +1,13 @@
+<?php require __DIR__. '/__connect_db.php';?>
+<?php include __DIR__. '/__html_head.php'; ?>
+<?php include __DIR__. '/__navbar.php'; ?> 
+
+<section class="transport">
+<h1>TRANS-port-ATION</h1>
+<select class="form-select destination-addr" aria-label="Default select example">
+  <option selected>Open this select menu</option>
+  <option value="1">One</option>
+</select>
+</section>
+<?php include __DIR__. '/__scripts.php'; ?> 
+<?php include __DIR__. '/__html_foot.php'; ?> 
