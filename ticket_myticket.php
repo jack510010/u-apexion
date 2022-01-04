5be4580b3adc3acce412c5d0ad6f07e4072f31f7
Tina0208 <96771110+Tina0208@users.noreply.github.com>
2022-01-04 18:12:31 +0800

    update

--- ticket_myticket.php
+++ ticket_myticket.php
@@ -18,14 +18,14 @@ $rows = $pdo->query($joinData)->fetchAll();
 <h2 class="my-3 text-white">我的訂票資訊</h2>
 <?php foreach($rows as $r):?>
   <div class="myticket-change d-flex justify-content-end">
-    <a class="myticket-edit-btn" href=""><i class="far fa-edit"></i>修改</a>
+    <a class="myticket-edit-btn" href="ticket_edit.php?sid=<?= $r['sid']?>"><i class="far fa-edit"></i>修改</a>
     <a class="myticket-delete-btn" href="ticket_delete.php?sid=<?= $r['sid'] ?>" onclick="return confirm('確定要刪除這筆資料嗎?')"><i class="fas fa-trash-alt"></i>刪除</a>
   </div>
   <div class="myticket-content d-flex">
     <img class="rocket-pic" src="./img/rocket.png" alt="">
   <div class="myticket-content-word w-75">
   <h4 class="py-2">My Ticket</h4>
-  <ul class="py-2">
+  <ul class="pt-2">
       <li class="">Name&emsp;<?= $r['name'] ?></li>
       <li class="">Date&emsp;<?= $r['flight_time'] ?></li>
       <li class="">Seat lvl&emsp;<?= $r['seat_sid'] ?></li>
@@ -34,7 +34,7 @@ $rows = $pdo->query($joinData)->fetchAll();
   </ul>
   </div>
   <div class="myticket-content-pic w-25">
-      <img src="./img/passtest.png" alt="">
+      <img src="./img/uploaded/<?= $r["passport"]?>" alt="">
   </div>
   </div>
   <br>
