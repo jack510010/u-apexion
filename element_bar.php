e2d3451ee02db6fcf8180f617cbd7981992631c8
Troykuojia <96803003+Troykuojia@users.noreply.github.com>
2022-01-04 18:45:21 +0800

    行程介紹
    行程介紹

---
+++ element_bar.php
@@ -0,0 +1,18 @@
+<?php
+require __DIR__. "/__connect_db.php";
+?>
+<?php require __DIR__. "/__html_head.php";?>
+<?php require __DIR__. "/__navbar.php";?>
+<!-- Example single danger button -->
+<div class="btn-group p-3 ">
+  <button type="button" class="btn btn-danger dropdown-toggle " data-bs-toggle="dropdown" aria-expanded="false">
+    旅程選擇
+  </button>
+  <ul class="dropdown-menu ">
+    <li><a class="dropdown-item" href="element.php">星座之旅(Element)</a></li>
+    <li><a class="dropdown-item" href="planet.php">星球之旅(Planet)</a></li>
+    <li><a class="dropdown-item" href="travel_insert.php">新增行程</a></li>
+  </ul>
+</div>
+<?php require __DIR__ . "/__scripts.php"; ?>
+<?php require __DIR__ . "/__html_foot.php"; ?>
