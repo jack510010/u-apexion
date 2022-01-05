1a29de77364776cd2cd9a4139c2d962d1d8db19d
Albert <jack25512909@gmail.com>
2022-01-05 19:09:46 +0800

    start

---
+++ cart.php
@@ -0,0 +1,9 @@
+<?php
+include __DIR__. "/__connect_db.php";
+
+$title = "購物車列表";
+?>
+<?php include __DIR__. "/__html_head.php"; ?>
+<?php include __DIR__. "/__navbar.php"; ?>
+<?php include __DIR__. "/__scripts.php"; ?>
+<?php include __DIR__. "/__html_foot.php"; ?>
