b2b8705e1227ae33786b7beb1ecf3a3ae6ccc0bc
Tina0208 <96771110+Tina0208@users.noreply.github.com>
2022-01-05 18:29:34 +0800

    update

--- ticket_edit.php
+++ ticket_edit.php
@@ -10,12 +10,13 @@ $sid = intval($_GET['sid']);
 $nowDataSql = "SELECT a.`sid`,a.`flight_time`,a.`trip_sid`,a.`seat_sid`,a.`member_count`,b.`ticket_sid`,b.`name`,b.`passport` FROM `ticket` a JOIN `member` b ON a.`sid` = b.`ticket_sid` WHERE a.`sid` = $sid";
 
 $nowDatarow = $pdo->query($nowDataSql)->fetch();
-
+$nowDatarowName = explode(",",$nowDatarow['name']);
 ?>
 <?php require __DIR__. "/__html_head.php";?>
 <?php require __DIR__. "/__navbar.php";?>
 
 <form class="ticket-form mt-3" name="ticketForm">
+<input type="hidden" name="sid" value="<?= $nowDatarow['sid'] ?>">
   <h2 class="mb-3">修改訂票資訊</h2>
   <div class="mb-3">
     <div class="d-flex align-items-center ticket-wrap">
@@ -33,11 +34,11 @@ $nowDatarow = $pdo->query($nowDataSql)->fetch();
   <div class="mb-3">
     <div class="d-flex align-items-center ticket-wrap">
     <label for="exampleInputEmail1" class="form-label  d-flex align-items-center justify-content-center align-self-stretch">旅遊行程</label>
-    <select class="ticket-form-select form-select form-control flex-fill" aria-label="Default select example" name="trip" required>
-  <option selected disabled>目前所選行程:&nbsp<?= $nowDatarow['trip_sid']?></option>
-  <option value="1">One</option>
-  <option value="2">Two</option>
-  <option value="3">Three</option>
+    <select id="select-trip" class="ticket-form-select form-select form-control flex-fill" aria-label="Default select example" name="trip" required>
+  <option  selected disabled>目前所選行程:&nbsp<?= $nowDatarow['trip_sid']?></option>
+  <option value="50元">One</option>
+  <option value="100元">Two</option>
+  <option value="150元">Three</option>
 </select>
     </div>
     <div class="ticket-incorrect"></div>
@@ -45,7 +46,7 @@ $nowDatarow = $pdo->query($nowDataSql)->fetch();
   <div class="mb-3">
     <div class="d-flex align-items-center ticket-wrap">
     <label for="exampleInputEmail1" class="form-label  d-flex align-items-center justify-content-center align-self-stretch">艙等</label>
-    <select required class="ticket-form-select form-select form-control flex-fill" aria-label="Default select example" name="seatLevel">
+    <select id="select-seat" required class="ticket-form-select form-select form-control flex-fill" aria-label="Default select example" name="seatLevel">
   <option selected disabled>目前所選艙等:&nbsp<?= $nowDatarow['seat_sid']?></option>
   <?php foreach ($seatrows as $r) { ?>
   <option ><?= $r['level'] ?></option>
@@ -64,7 +65,7 @@ $nowDatarow = $pdo->query($nowDataSql)->fetch();
   </div>
   <div class="member-input">
   </div>
-  <button type="submit" class="ticketBtn btn-primary px-4 py-1" onclick="sendTicketForm(); return false">送出</button>
+  <button type="submit" class="ticketBtn btn-primary px-4 py-1" onclick="sendTicketForm(); return false">修改</button>
 </form>
 
 <?php require __DIR__. "/__scripts.php";?>
@@ -74,7 +75,7 @@ $nowDatarow = $pdo->query($nowDataSql)->fetch();
     const memberValue = `<div class="mb-3">
     <div class="d-flex align-items-center ticket-wrap">
     <label for="exampleInputEmail1" class="memberName form-label  d-flex align-items-center justify-content-center align-self-stretch">成員姓名</label>
-    <input type="" class="form-control flex-fill" id="exampleInputEmail1" aria-describedby="emailHelp" name="member[]" placeholder="請輸入護照英文名字" value="<?= $nowDatarow['name'] ?>">
+    <input type="" class="form-control flex-fill" id="exampleInputEmail1" aria-describedby="emailHelp" name="member[]" placeholder="請輸入護照英文名字" value="<?= $nowDatarowName[0]?>">
     </div>
     <div class="ticket-incorrect"></div>
   </div><div class="mb-3">
@@ -145,4 +146,28 @@ const memberInput = `<div class="mb-3">
         document.querySelector(".ticketBtn").style= "display: block";
 }
 
+function sendTicketForm(){
+  const fd = new FormData(document.ticketForm);
+
+  fetch("ticket_edit_api.php",{
+    method: 'POST',
+    body: fd,
+  }).then(r=>r.json())
+  .then(txt => {
+    alert(txt.sid);
+    location.href = "ticket_myticket.php";
+});
+}
+
+const select = document.querySelector("#select");
+select.addEventListener('change', showValue);
+
+function showValue(e){
+  console.log(e.target.value);
+};
+
 </script>
+
+設定監聽器 
+監聽下拉選單value改變 or 使用者點選特定項目
+console.log(value)
