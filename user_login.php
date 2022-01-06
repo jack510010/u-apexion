fbf739be1c370ee75081355cb009fecfd4848097
Sam Wang <flyknit101@gamil.com>
2022-01-06 17:43:01 +0800

    user update

--- user_login.php
+++ user_login.php
@@ -6,7 +6,7 @@ session_start();
 <?php require __DIR__ . "/__navbar.php"; ?>
 <nav class="navbar navbar-expand-lg navbar-light pt-3 shadow ">
     <div class="container-fluid"><i class="fas fa-laptop-house text-warning"></i>
-        <a class="navbar text-warning " href="user_list.php" style="text-decoration:none;"></a>
+        <a class="navbar text-warning " href="#" style="text-decoration:none;"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
