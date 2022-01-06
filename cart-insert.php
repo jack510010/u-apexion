286a92032efca80c82a50e3075436df316e90a8a
Albert <jack25512909@gmail.com>
2022-01-06 14:17:14 +0800

    insert

---
+++ cart-insert.php
@@ -0,0 +1,10 @@
+<?php
+include __DIR__ . "/__connect_db.php";
+$title = "新增購物車資料";
+$pageName = "insert"; // 新增資料頁
+?>
+
+<?php include __DIR__ . "/__html_head.php" ?>
+<?php include __DIR__ . "/__navbar.php" ?>
+<?php include __DIR__ . "/__scripts.php" ?>
+<?php include __DIR__ . "/__html_foot.php" ?>
