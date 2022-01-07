0e9b7f2df9bfd7ba9022934bdad7105e9c5cdafa
Jhih Ting <tina23472002@gmail.com>
2022-01-07 11:51:53 +0800

    產品照片上傳轉移成功

--- product_new_api.php
+++ product_new_api.php
@@ -13,7 +13,7 @@ $style = $_POST['style'] ?? '';
 $size = $_POST['size'] ?? '';
 $quantity = $_POST['quantity'] ?? '';
 $price = $_POST['price'] ?? '';
-$imgName = implode(",", $img['name']);
+// $imgName = implode(",", $img['name']);
 
 //檢查欄位資料
 if (empty($product_name)) {
@@ -30,34 +30,36 @@ if (empty($quantity)) {
     exit;
 }
 
-$sql = "INSERT INTO `product`(`category`, `product_name`, `img`, `style`,
- `size`, `quantity`, `price`, `create_date`, `update_date`) VALUES (?,?,?,?,?,?,?,NOW(),NOW())";
 
-$stmt = $pdo->prepare($sql);
-$stmt->execute([
-    $category,
-    $product_name,
-    $imgName,
-    $style = $_POST['style'] ?? '',
-    $size = $_POST['size'] ?? '',
-    $quantity,
-    empty($_POST['price']) ? NULL : $_POST['price'],
-
-]);
 
 header('Content-Type: application/json');
 
 $upload_folder = __DIR__ . '/img/product_img';
 
-if(! empty($_FILES['img'])) {
+if (!empty($_FILES['img'])) {
     $filename = sha1($_FILES['img']['name'] . rand());
-    $target = $upload_folder. '/'. $filename;
-    if( move_uploaded_file($_FILES['img']['tmp_name'], $target)){
+    $target = $upload_folder . '/' . $filename;
+    if (move_uploaded_file($_FILES['img']['tmp_name'], $target)) {
         $output['success'] = true;
+        $output['filename'] = $filename;
+
+        $sql = "INSERT INTO `product`(`category`, `product_name`, `img`, `style`,
+ `size`, `quantity`, `price`, `create_date`, `update_date`) VALUES (?,?,?,?,?,?,?,NOW(),NOW())";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute([
+            $category,
+            $product_name,
+            $filename,
+            $style = $_POST['style'] ?? '',
+            $size = $_POST['size'] ?? '',
+            $quantity,
+            empty($_POST['price']) ? NULL : $_POST['price'],
+
+        ]);
     } else {
         $output['error'] = '無法移動檔案';
     }
-
 } else {
     $output['error'] = '沒有上傳檔案';
 }
