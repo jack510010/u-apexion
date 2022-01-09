134632296ea35f92e819ce27de99852ee6491040
Sam Wang <flyknit101@gamil.com>
2022-01-09 18:13:11 +0800

    增減pageName

--- element_index.php
+++ element_index.php
@@ -1,25 +1,28 @@
 <?php
 require __DIR__ . "/__connect_db.php";
+
+$pageName = "element";
 ?>
 
 <style>
-  img {
+img {
     border-radius: 60px;
-  }
-  h1{
+}
+
+h1 {
     color: White;
-    text-align:center;
-  }
+    text-align: center;
+}
 </style>
 <?php require __DIR__ . "/__html_head.php"; ?>
 <?php require __DIR__ . "/__navbar.php"; ?>
 <!-- Example single danger button -->
-<h1 >行程選擇</h1>
+<h1>行程選擇</h1>
 <div class="container">
-  <div class="row">
-    <div class="col mb-5"><a href="element.php"><img src="./img/639.png" alt="" ></a></div>
-    <div class="col mb-5"><a href="https://youtu.be/072tU1tamd0"><img src="./img/638.png" alt="" ></a></div>
-  </div>
+    <div class="row">
+        <div class="col mb-5"><a href="element.php"><img src="./img/639.png" alt=""></a></div>
+        <div class="col mb-5"><a href="https://youtu.be/072tU1tamd0"><img src="./img/638.png" alt=""></a></div>
+    </div>
 </div>
 
 
